86972b9e6018214c9b5c831d952dc7e79ca1d16f
willnet <netwillnet@gmail.com>
2014-06-29 22:32:01 +0900

    Add functon getting hashes are not resolved yet

--- lib/readme_tracker/cli.rb
+++ lib/readme_tracker/cli.rb
@@ -1,7 +1,8 @@
 require 'yaml'
+require 'fileutils'
 
 class ReadmeTracker::Cli
-  attr_accessor :yaml_path
+  attr_accessor :yaml_path, :bash_hash
 
   def self.run(config = {})
     new(config).run
@@ -9,10 +10,21 @@ class ReadmeTracker::Cli
 
   def initialize(config)
     @yaml_path = config[:yaml_path] || './readme_tracker.yml'
+    hash_path = config[:hash_path] || './hash.txt'
+    @base_hash = File.read(hash_path).chomp
   end
 
   def run
     yaml = YAML.load(File.read(yaml_path))
+    repository_name = yaml['repo'].split('/')[1]
     system "git clone git@github.com:#{yaml['repo']}.git"
+
+    FileUtils.cd repository_name
+    log = `git log --oneline README.md`
+    all_hashes = log.each_line.map { |line| line.strip[0..6] }
+    issue_hashes = all_hashes.take_while { |hash| hash != base_hash }
+
+    # TODO: make issues on user's repository from issue_hashes
+    # TODO: overwrite hash.txt
   end
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,9 +4,10 @@ describe ReadmeTracker::Cli do
   describe '#run' do
     context 'when pass yaml_path as argument' do
       it "should git clone its repository" do
-        path = File.expand_path('../support/sample.yml', __FILE__)
-        cli = ReadmeTracker::Cli.new(yaml_path: path)
-        expect(cli).to receive(:system).with("git clone git@github.com:willnet/readme_tracker.git")
+        yaml_path = File.expand_path('../support/sample.yml', __FILE__)
+        hash_path = File.expand_path('../support/hash.txt', __FILE__)
+        cli = ReadmeTracker::Cli.new(yaml_path: yaml_path, hash_path: hash_path)
+        expect(cli).to receive(:system).with("git clone git@github.com:willnet/readme_tracker.git").and_call_original
         cli.run
       end
     end
